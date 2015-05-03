513a61e4ab00209e147d17954d82c6f20d77c871
Eduardo F. Casal <educasal@users.noreply.github.com>
2015-05-03 21:26:04 +0200

    A little testing and persistencd

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,15 @@
  * Controller of the yeomanTodoAppApp
  */
 angular.module('yeomanTodoAppApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'Processing'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+  	var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+    	localStorageService.set('todos', $scope.todos);
+    }, true);
 
     $scope.addToDo = function () {
     	if($scope.todo && ($scope.todos.indexOf($scope.todo) === -1)){
@@ -24,6 +26,6 @@ angular.module('yeomanTodoAppApp')
     };
 
     $scope.removeToDo = function (index) {
-    	$scope.todos.splice(index, 1)
+    	$scope.todos.splice(index, 1);
     };
   });
