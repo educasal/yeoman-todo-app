7fc227cadf57732ab39e101374f645c58f09b810
Eduardo F. Casal <educasal@users.noreply.github.com>
2015-05-03 18:43:40 +0200

    To-do management

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # yeoman-todo-app
 
+Codelab tutorial
+
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.11.1.
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,21 @@
  */
 angular.module('yeomanTodoAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+    $scope.todos = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'Processing'
     ];
+
+    $scope.addToDo = function () {
+    	if($scope.todo && ($scope.todos.indexOf($scope.todo) === -1)){
+    		$scope.todos.push($scope.todo);	
+    		$scope.todo = '';    		
+    	}
+    };
+
+    $scope.removeToDo = function (index) {
+    	$scope.todos.splice(index, 1)
+    };
   });
